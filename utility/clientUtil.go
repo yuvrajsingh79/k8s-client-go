a87e733e8a705cd9e6a072ebb75f9eb3417816c3
yuvrajsingh79 <singhyuvraj79@gmail.com>
2020-06-02 11:31:02 +0530

    origin/master
    added utility pkg for crd

--- utility/clientUtil.go
+++ utility/clientUtil.go
@@ -6,8 +6,10 @@ import (
 	"os"
 	"path/filepath"
 
-	// apiextension "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
-	// myresourceclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/versioned"
+	myresourceclientset "client-go/k8s-client-go/pkg/client/clientset/versioned"
+
+	"github.com/micro/go-micro/util/log"
+	apiextension "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -53,35 +55,35 @@ func GetClientset() (*kubernetes.Clientset, error) {
 }
 
 //GetKubeClient functions helps to connect kubeclient for Custom Resource
-// func GetKubeClient() (apiextension.Interface, myresourceclientset.Interface, kubernetes.Interface) {
-// 	// construct the path to resolve to `~/.kube/config`
-// 	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
+func GetKubeClient() (apiextension.Interface, myresourceclientset.Interface, kubernetes.Interface) {
+	// construct the path to resolve to `~/.kube/config`
+	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
 
-// 	// create the config from the path
-// 	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-// 	if err != nil {
-// 		log.Fatalf("getClusterConfig: %v", err)
-// 	}
+	// create the config from the path
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		log.Fatalf("getClusterConfig: %v", err)
+	}
 
-// 	// generate the client based off of the config
-// 	client, err := apiextension.NewForConfig(config)
-// 	if err != nil {
-// 		log.Fatalf("getClusterConfig: %v", err)
-// 	}
+	// generate the client based off of the config
+	client, err := apiextension.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("getClusterConfig: %v", err)
+	}
 
-// 	kubeClient, err := kubernetes.NewForConfig(config)
-// 	if err != nil {
-// 		log.Fatalf("getClusterConfig: %v", err)
-// 	}
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("getClusterConfig: %v", err)
+	}
 
-// 	myresourceClient, err := myresourceclientset.NewForConfig(config)
-// 	if err != nil {
-// 		log.Fatalf("getClusterConfig: %v", err)
-// 	}
+	myresourceClient, err := myresourceclientset.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("getClusterConfig: %v", err)
+	}
 
-// 	log.Info("Successfully constructed k8s client")
-// 	return client, myresourceClient, kubeClient
-// }
+	log.Info("Successfully constructed k8s client")
+	return client, myresourceClient, kubeClient
+}
 
 // PrettyString returns the prettified string of the interface supplied. (If it can)
 func PrettyString(in interface{}) string {
